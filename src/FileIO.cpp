c6ad7305c1259356cc6a04bb688187be1241fd58
Juergen Bis <juergen.bis@rise-world.com>
2018-03-08 09:37:18 +0100

    try some VectorHandling, File IO and Delimiter Stuff

--- src/FileIO.cpp
+++ src/FileIO.cpp
@@ -14,75 +14,79 @@
 #include <string>
 #include <vector>
 #include <stdlib.h> // atoi
+#include <cstdlib>
+
+#include "Weapon.hpp"
 
 //using namespace std;
 
 const std::string currentDate();
 
-typedef unsigned long int uint16_t;
-typedef unsigned short uint8_t;
+//typedef unsigned long int uint16_t;
+//typedef unsigned short uint8_t;
 
-
-typedef enum {
-	sword1H = 0x00,
-	sword2H,
-	axe1H,
-	axe2H,
-	knife,
-	stick,
-	staff
-} meleeWeapon_t;
-
-typedef enum {
-	melee = 0x00,
-	range,
-	wand,
-	mage
-}weaponType_t;
-
-typedef struct {
-	uint16_t id;
-	uint8_t type;
-	uint8_t subType;
-	std::string name;
-	uint16_t dmg;
-	uint16_t weight;
-	float speed;
-	uint8_t hands;
-	uint8_t ranged;
-}weaponStats_s;
 
 int main() {
-	std::cout << "!!!Hello World!!!" << std::endl; // prints !!!Hello World!!!
 	std::stringstream _ss;
 	std::string readBuf;
-	std::string delimiter = ";";
-	size_t pos;
-
-	weaponStats_s activeWeapon;
+	char delimiter = ';';
+	std::string delimiterEOL = "#";
+	std::vector <Weapon> weaponType;
+	Weapon activeWeapon (0,0,0,"",0,0,0,0,0);
+	uint8_t numberOfLines = 0;
 
 	std::ifstream fHandlerWeapons("weapon");
-	if (fHandlerWeapons.is_open()) {
+	if (!fHandlerWeapons.is_open()) {
+		printf("file not found!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	while (getline (fHandlerWeapons,readBuf)) {
+		printf("ID-> %d\n",atoi(readBuf.substr(0, readBuf.find(delimiter)).c_str()));
+		weaponType[numberOfLines];
+		activeWeapon.setID(atoi(readBuf.substr(0, readBuf.find(delimiter)).c_str()));
+
+		printf("Type-> %d\n",atoi(readBuf.substr(2, readBuf.find(delimiter)).c_str()));
+		printf("SubType-> %d\n",atoi(readBuf.substr(4, readBuf.find(delimiter)).c_str()));
+		printf("Name-> %s\n",readBuf.substr(6, readBuf.find(delimiter)).c_str());
+
+		//activeWeapon.setID(atoi(readBuf.substr(0, readBuf.find(delimiter)).c_str()));
+		//weaponType.push_back(activeWeapon);
+		++numberOfLines;
+	}
 
 
+	printf("number of lines in weapon file: %d\n",numberOfLines);
+	printf("Size of Vector %d\n", weaponType.size());
 
-		while (getline (fHandlerWeapons,readBuf)) {
-			()pos =
-			activeWeapon.id = atoi(readBuf.substr(0,readBuf.find(delimiter)).c_str());
+	std::cout << "ID in Vector " << weaponType.at(0) << ":" << activeWeapon.getID() << std::endl;
+	printf("ID in Vector %d: %d\n", weaponType[1], activeWeapon.getID());
+	printf("ID in Vector %d: %d\n", weaponType[2], activeWeapon.getID());
+	printf("ID in Vector %d: %d\n", weaponType[3], activeWeapon.getID());
+
+	while(numberOfLines!=0) {
+
+		printf("vector entries: %d\n",weaponType[numberOfLines]);
+		numberOfLines--;
+	}
+
+
+ /*
+
 			activeWeapon.type = atoi(readBuf.substr(1,readBuf.find(delimiter)).c_str());
 			activeWeapon.subType = atoi(readBuf.substr(2,readBuf.find(delimiter)).c_str());
 			activeWeapon.name = readBuf.substr(6,readBuf.find(delimiter));
 			std::cout << readBuf << "\n";
-		}
-		fHandlerWeapons.close();
-	}
+			i++;
+
+	fHandlerWeapons.close();
 
 	std::cout << "ID: " << activeWeapon.id << "\n";
 	std::cout << "Type: " << activeWeapon.type << "\n";
 	std::cout << "SubType: " << activeWeapon.subType << "\n";
 	std::cout << "Name: " << activeWeapon.name << "\n";
 
-
+*/
 
 	//_ss.str("");
 	_ss << currentDate()
